f9c973e76b305b69f0202945227ec9ed868cd0df
4tk666 <vhfxhy5356@yahoo.co.jp>
2024-07-14 14:58:46 +0900

    storybookのバージョンアップデート

--- src/components/presentations/Card/Card.stories.tsx
+++ src/components/presentations/Card/Card.stories.tsx
@@ -1,19 +1,37 @@
 import { Meta, StoryObj } from '@storybook/react';
 import ComCard from '.';
 
-const meta = {
+const meta: Meta<typeof ComCard> = {
   title: 'Card',
   component: ComCard,
+  argTypes: {
+    color: {
+      control: 'radio',
+      options: ['white', 'gray'],
+    },
+    children: {
+      control: 'text',
+    },
+    disableRounded: {
+      control: 'boolean',
+    },
+    disablePadding: {
+      control: 'boolean',
+    },
+    className: {
+      control: 'text',
+    },
+  },
 } satisfies Meta<typeof ComCard>;
 
 export default meta;
 
 type Story = StoryObj<typeof ComCard>;
 
-const RenderCard = () => {
-  return <ComCard>カード</ComCard>;
-};
-
 export const Card: Story = {
-  render: () => <RenderCard />,
+  args: {
+    color: 'white',
+    children: 'カード',
+  },
+  render: (args) => <ComCard {...args} />,
 };
